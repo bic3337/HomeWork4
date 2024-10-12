59f3dba58d83b2178c707efa935d897dbf904f02
ПК <bic3337@gmail.com>
2024-10-12 17:27:12 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,16 @@ public class Main {
         int clientDevaysYear = 2013;
         if (clientOS == 0 && clientDevaysYear >= 2015) {
             System.out.println("Установите приложение для IOS по ссылке.");
-        } if (clientOS == 1 && clientDevaysYear >= 2015) {
+        }
+
+        if (clientOS == 1 && clientDevaysYear >= 2015) {
             System.out.println("Установите приложение для Android по ссылке.");
-        } if (clientOS == 0 && clientDevaysYear < 2015) {
+        }
+
+        if (clientOS == 0 && clientDevaysYear < 2015) {
             System.out.println("Установите облегченную версию для IOS по ссылке.");
         } else
             System.out.println("Установите облегченную версию для Android по ссылке.");
-
-
 
 
     }
